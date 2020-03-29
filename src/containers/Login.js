c31c979accff4eef0c215d68f9e3a394be4c18e3
Thomas <wyatsky@gmail.com>
2020-03-30 09:52:25 +1000

    Redirect to home after login using prop.history.push()

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -18,6 +18,7 @@ export default function Login(props){
         try {
             await Auth.signIn(email, password);
             props.userHasAuthenticated(true);
+            props.history.push("/");
         }catch(err){
             alert(err.message);
         }
