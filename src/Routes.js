d1081f5cd4327c1119284d622516872d79ff00f2
Thomas <wyatsky@gmail.com>
2020-03-29 17:24:35 +1000

    Add 404 in Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
     return (
@@ -8,6 +9,8 @@ export default function Routes() {
         {/* Switch is used to render the first matching route that is defined within it. */}
         {/* exact makes sure that this component matches "/" only. */}
             <Route path="/" exact component={Home} />
+            {/* catch all unmatched routes */}
+            <Route component={NotFound} />
         </Switch>
     );
 }
