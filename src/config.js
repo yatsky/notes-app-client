0d2a648edd53aca17772a73df68845b705eca94b
Thomas <wyatsky@gmail.com>
2020-03-29 17:25:28 +1000

    Add AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "yaoni-notes-app-uploads-tutorial"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://fqxti1tdlg.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_fEIAnjEtt",
+    APP_CLIENT_ID: "2smkgslrjtd838sm8o5j1b6rfg",
+    IDENTITY_POOL_ID: "ap-southeast-2:f244ffff-0260-4174-ac70-102209f7025a"
+  }
+};
