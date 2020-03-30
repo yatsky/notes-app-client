2cd759cab71a5d09c7c3036a0238bd628741db92
Thomas <wyatsky@gmail.com>
2020-03-30 16:26:24 +1000

    Use List API to list all notes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,56 @@
-import React, { useState } from "react";
-import { PageHeader, ListGroup} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./Home.css";
-import { PresignedPost } from "aws-sdk/clients/s3";
+import { API } from "aws-amplify";
 
 export default function Home(props) {
     const [notes, setNotes] = useState([]);
     const [isLoading, setIsLoading]=useState(true);
 
+    useEffect(() => {
+        async function onLoad() {
+            if (!props.isAuthenticated){
+                return;
+            }
+            try{
+                const notes = await loadNotes();
+                setNotes(notes);
+            }catch(err){
+                alert(err);
+            }
+            setIsLoading(false);
+        }
+
+        onLoad();
+        // we only want to run this useEffect() when
+        // props.isAuthenticated changes
+    }, [props.isAuthenticated]);
+
+    function loadNotes(){
+        return API.get("notes", "/notes");
+    }
+
     function renderNotesList(notes){
-        return null;
+        // The empty object {} serves as the first LinkContainer
+        // for creating a new note
+        return [{}].concat(notes).map((note, i) =>
+        i !== 0 ? (
+            <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+                <ListGroupItem header={note.content.trim().split("\n")[0]}>
+                    {"Created: " + new Date(note.createdAt).toLocaleString()}
+                </ListGroupItem>
+            </LinkContainer>
+        ) : (
+            <LinkContainer key="new" to="/notes/new">
+                <ListGroupItem>
+                    <h4>
+                        <b>{"\uFF0B"}</b> Create a new note
+                    </h4>
+                </ListGroupItem>
+            </LinkContainer>
+        )
+        );
     }
 
     function renderLander(){
@@ -33,6 +75,7 @@ export default function Home(props) {
         );
     }
 
+            console.log(props.isAuthenticated);
     return (
         <div className="Home">
             {props.isAuthenticated ? renderNotes() : renderLander()}
