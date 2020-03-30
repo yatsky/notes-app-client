48eec7c01973edf808ea6b1379543803b7afd13b
Thomas <wyatsky@gmail.com>
2020-03-30 13:58:40 +1000

    Add Signup page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import AppliedRoute from "./components/AppliedRoute";
 
 export default function Routes({ appProps }) {
@@ -12,6 +13,7 @@ export default function Routes({ appProps }) {
         {/* exact makes sure that this component matches "/" only. */}
             <AppliedRoute path="/" exact component={Home} appProps={appProps} />
             <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+            <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
             {/* catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
---
+++ src/containers/Signup.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+    HelpBlock,
+    FormGroup,
+    FormControl,
+    ControlLabel
+} from "react-bootstrap";
+
+import LoaderButton from "../components/LoaderButton";
+import { useFormFields } from "../libs/hooksLib";
+import "./Signup.css";
+
+export default function Signup(props) {
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        confirmationCode: ""
+    });
+    const [newUser, setNewUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    function validateForm(){
+        return (
+            fields.email.length > 0 &&
+            fields.password.length > 0 &&
+            fields.password === fields.confirmPassword
+        );
+    }
+
+    function validateConfirmationForm(){
+        return fields.confirmationCode.length > 0;
+    }
+
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        setIsLoading(true);
+        setNewUser("test");
+        setIsLoading(false);
+    }
+
+    async function handleConfirmationSubmit(e){
+        e.preventDefault();
+        setIsLoading(true);
+    }
+
+    function renderConfirmationForm() {
+        return (
+            <form onSubmit={handleConfirmationSubmit}>
+                <FormGroup controlId="confirmationCode" bsSize="large">
+                    <ControlLabel>Confirmation Code</ControlLabel>
+                    <FormControl
+                    autoFocus
+                    type="tel"
+                    onChange={handleFieldChange}
+                    value={fields.confirmationCode}
+                    />
+                    <HelpBlock>Please check your email for the code.</HelpBlock>
+                </FormGroup>
+                <LoaderButton
+                block
+                type="submit"
+                bsSize="large"
+                isLoading={isLoading}
+                disabled={!validateConfirmationForm()}
+                >
+                    Verify
+                </LoaderButton>
+            </form>
+        );
+    }
+
+    function renderForm() {
+        return (
+        <form onSubmit={handleSubmit}>
+            <FormGroup controlId="email" bsSize="large">
+                <ControlLabel>Email</ControlLabel>
+                <FormControl
+                autoFocus
+                type="email"
+                value={fields.email}
+                onChange={handleFieldChange}
+                />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                type="password"
+                value={fields.password}
+                onChange={handleFieldChange}
+                />
+            </FormGroup>
+            <FormGroup controlId="confirmPassword" bsSize="large">
+                <ControlLabel>Confirm Password</ControlLabel>
+                <FormControl
+                type="password"
+                value={fields.confirmPassword}
+                onChange={handleFieldChange}
+                />
+            </FormGroup>
+            <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+            >
+                Sign up
+            </LoaderButton>
+        </form>
+        );
+    }
+
+    return (
+        <div className="Signup">
+            {newUser === null ? renderForm() : renderConfirmationForm()}
+        </div>
+    );
+}
