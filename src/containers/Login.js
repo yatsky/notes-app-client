70bb662ca38acba4660f9040c7a5db0524adcea7
Thomas <wyatsky@gmail.com>
2020-03-30 13:33:41 +1000

    Use userFormFields custom Hook

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 import LoaderButton from "../components/LoaderButton";
+import { useFormFields } from "../libs/hooksLib";
 
 
 export default function Login(props){
     // we use hooks to store/update variables
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: ""
+    });
 
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
+        return fields.email.length > 0 && fields.password.length > 0;
     }
     async function handleSubmit(e) {
         e.preventDefault();
 
         setIsLoading(true);
         try {
-            await Auth.signIn(email, password);
+            await Auth.signIn(fields.email, fields.password);
             props.userHasAuthenticated(true);
             props.history.push("/");
         }catch(err){
@@ -37,15 +41,17 @@ export default function Login(props){
                     <FormControl
                     autoFocus
                     type="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
+                    value={fields.email}
+                    // We could also write
+                    // onChange={handleFieldChange}
+                    onChange={e => handleFieldChange(e)}
                     />
                 </FormGroup>
                 <FormGroup controlId="password" bsSize="large">
                     <ControlLabel>Password</ControlLabel>
                     <FormControl
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
+                    value={fields.password}
+                    onChange={e => handleFieldChange(e)}
                     type="password"
                     />
                 </FormGroup>
