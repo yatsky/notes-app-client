9edaa6d3229dcbce4bd4a4a8460c73f8b24fe9c0
Thomas <wyatsky@gmail.com>
2020-03-30 16:51:29 +1000

    Display single note

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import AppliedRoute from "./components/AppliedRoute";
 
 export default function Routes({ appProps }) {
@@ -16,6 +17,7 @@ export default function Routes({ appProps }) {
             <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
             <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
             <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+            <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
             {/* catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
