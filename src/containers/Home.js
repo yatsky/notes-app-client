d8176fde5a7219fed00775f32009f5198b52df91
Thomas <wyatsky@gmail.com>
2020-03-30 15:58:55 +1000

    Page to list all notes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { PageHeader, ListGroup} from "react-bootstrap";
 import "./Home.css";
+import { PresignedPost } from "aws-sdk/clients/s3";
 
-export default function Home() {
-    return (
+export default function Home(props) {
+    const [notes, setNotes] = useState([]);
+    const [isLoading, setIsLoading]=useState(true);
+
+    function renderNotesList(notes){
+        return null;
+    }
+
+    function renderLander(){
+        return (
         <div className="Home">
             <div className="lander">
                 <h1>Scratch</h1>
                 <p>A modernised note taking app</p>
             </div>
         </div>
-    )
+        );
+    }
+
+    function renderNotes(){
+        return (
+            <div className="notes">
+                <PageHeader>Your Notes</PageHeader>
+                <ListGroup>
+                    {!isLoading && renderNotesList(notes)}
+                </ListGroup>
+            </div>
+        );
+    }
+
+    return (
+        <div className="Home">
+            {props.isAuthenticated ? renderNotes() : renderLander()}
+        </div>
+    );
 }
